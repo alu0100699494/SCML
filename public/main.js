756db6c275c2c06804f65f523f41e3ba4ac6ceb7
Daniel Herzog <alu0100699494@ull.edu.es>
2014-05-04 15:31:05 +0100

    Añadido botón de descarga para descargar ficheros HTML. Además, se ha modificado el fichero main.js para que funcione de manera correcta con CodeMirror.

--- public/main.js
+++ public/main.js
@@ -1,11 +1,26 @@
 $(document).ready(function() {
   $('#parse').click(function() {
+    var editor = $('.CodeMirror')[0].CodeMirror;
+	  var source = editor.getValue();
+    
     try {
-      var result = pl0.parse($('#input').val());
+      //var result = pl0.parse($('#input').val());
+      var result = pl0.parse(source);
+      
       $('#output').html(JSON.stringify(result,undefined,2));
     } catch (e) {
       $('#output').html('<div class="error"><pre>\n' + String(e) + '\n</pre></div>');
     }
+  });
+  
+  $('#download').click(function() {
+    var editor = $('.CodeMirror')[1].CodeMirror;
+	  var source = editor.getValue();
+    
+    var pom = document.createElement('a');
+    pom.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(source));
+    pom.setAttribute('download', 'index.html');
+    pom.click();
   });
 
   $("#examples").change(function(ev) {
