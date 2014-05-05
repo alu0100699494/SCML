941a19b30127934d6dc92395f504ec718afeee60
Daniel Herzog <alu0100699494@ull.edu.es>
2014-05-05 20:01:30 +0100

    Eliminado el escapado basura (externo) de entidades HTML.

--- public/main.js
+++ public/main.js
@@ -12,17 +12,6 @@ $(document).ready(function() {
       var result = generate_code(tree);
       var html_output = $('.CodeMirror')[1].CodeMirror;
       
-      // mostrar para codemirror
-      var entities = {
-        '&lt;': '<',
-        '&gt;': '>',
-        '&quot;': '"',
-        '&apos;': "'",
-        '&amp;': '&'
-      };
-
-      result = result.replace(/\\/g, '').replace(/(&lt;|&gt;|&quot;|&apos;|&amp;)/g, function(tag) { return entities[tag] || tag; })
-      
 	    html_output.setValue(result);
       
       //$('#output').text(result);
